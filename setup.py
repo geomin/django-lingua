f1ec4047b4e89a5a5710e6607f87cafa478017ab
Georg Kasmin <gk@displayboy.com>
2011-04-28 15:10:57 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='ligua',
+    version='0.8.2',
+    url='http://github.com/geomin/django-lingua',
+    maintainer='Georg Kasmin',
+    maintainer_email='georg@aquarianhouse.com',
+    description='Djano model translation on basis of i18n',
+    packages=[],
+    cmdclass={},
+    data_files=[],
+    classifiers=['License :: OSI Approved :: BSD License',
+                 'Intended Audience :: Developers',
+                 'Programming Language :: Python',
+                 'Topic :: I18N :: Internalization'],
+    package_data={},
+    zip_safe=False,
+)
